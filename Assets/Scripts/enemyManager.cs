bbc89fea3353719671a8924018ead8e50029d8fc
Brozerker <chris.randall619@yahoo.com>
2015-12-15 01:14:30 -0800

    movement

--- Assets/Scripts/enemyManager.cs
+++ Assets/Scripts/enemyManager.cs
@@ -7,26 +7,33 @@ struct Node {
 }
 
 public class enemyManager : MonoBehaviour {
-    private Vector2 playerPos;
+    private Transform target;
     private Vector2 toMove;
     private stateMachine FSM;
     private GameObject[] nodes;
-    public float speed = 3.0f;
+    public float speed = 0.5f;
     public ArrayList nodesToCopy;
+    public float range = 15f;
     
 	// Use this for initialization
 	void Start () {
         nodes = GameObject.FindGameObjectsWithTag("Node");
-	}
-
-    void Update() {
-        //FSM.Update(this, nodes);
     }
 
     // Update is called once per frame
-    void FixedUpdate() {
-        playerPos = GameObject.FindGameObjectWithTag("Player").transform.position;
-        GetComponent<Rigidbody2D>().velocity = new Vector2(playerPos.x * speed, playerPos.y * speed);
-        Debug.Log(playerPos.ToString(), gameObject);
-	}
+    void Update() {
+        //FSM.Update(this, nodes);
+        target = GameObject.FindWithTag("Player").transform;
+        Debug.Log(Vector2.Distance(transform.position, target.position));
+       // if (Vector2.Distance(transform.position, target.position) < range) {
+            Debug.DrawLine(transform.position, target.position);
+            //Debug.DrawLine(transform.position);
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        //}
+        Vector2 moveDirection = GetComponent<Rigidbody2D>().velocity;
+        if (moveDirection != Vector2.zero) {
+            float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+    }
 }
