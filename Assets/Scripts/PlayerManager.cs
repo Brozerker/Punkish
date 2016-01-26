4d272b4e2febe655d9c9e7a47375292420f04edf
WillBowden <william.t.bowden@Gmail.com>
2016-01-26 15:18:04 -0800

    Inventory and Item Pickup
    Code for inventory and item pickup

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -5,6 +5,15 @@ public class PlayerManager : MonoBehaviour {
     public float speed = 0.5f;
     private float moveX = 0;
     private float moveY = 0f;
+    public Inventory inventory;
+
+
+
+
+    /// <summary>
+    /// Handles the player's collision
+    /// </summary>
+    /// <param name="other"></param>
 
 	// Use this for initialization
 	void Start () {
@@ -30,10 +39,37 @@ public class PlayerManager : MonoBehaviour {
         //if (GetComponent<Rigidbody2D>().velocity.y > 0) {
         //    GetComponent<Transform>().Rotate(-90, 0, 0);
         //}
+         if (Input.GetKeyDown(KeyCode.B))
+         {
+             inventory.Open();
+
+         }
 	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Item") //If we collide with an item that we can pick up
+        {
+            inventory.AddItem(other.GetComponent<Item>()); //Adds the item to the inventory.
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Item") //If we collide with an item that we can pick up
+        {
+            inventory.AddItem(collision.gameObject.GetComponent<Item>()); //Adds the item to the inventory.
+
+            Destroy(collision.gameObject);
+        }
+    }
+
     void FixedUpdate() {
         moveX = Input.GetAxis("Horizontal");
         moveY = Input.GetAxis("Vertical");
         GetComponent<Rigidbody2D>().velocity = new Vector2(moveX * speed, moveY * speed);
     }
+
+    
+
 }
