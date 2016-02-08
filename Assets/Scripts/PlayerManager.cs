faa7ce46d3c8815cec9b620b5f7679a98db771b2
WillBowden <william.t.bowden@Gmail.com>
2016-02-07 18:39:50 -0800

    Added Attack Animation
    Pressing the spacebar in any direction will play the attack sprite

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -7,6 +7,7 @@ public class PlayerManager : MonoBehaviour {
     private float moveY = 0f;
     public Inventory inventory;
 
+    public Sprite up, down, left, right, attk_up, attk_down, attk_left, attk_right;
 
 
 
@@ -25,7 +26,7 @@ public class PlayerManager : MonoBehaviour {
          Vector2 moveDirection = GetComponent<Rigidbody2D>().velocity;
          if (moveDirection != Vector2.zero) {
              float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
-             transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+             //transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
          }
         //if (GetComponent<Rigidbody2D>().velocity.x < 0) {
         //    GetComponent<Sprite>(). .Rotate(-90,0,0);
@@ -39,6 +40,20 @@ public class PlayerManager : MonoBehaviour {
         //if (GetComponent<Rigidbody2D>().velocity.y > 0) {
         //    GetComponent<Transform>().Rotate(-90, 0, 0);
         //}
+
+        //Move Direction
+         if (Input.GetKey("left"))              { GetComponent<SpriteRenderer>().sprite = left; }
+         if (Input.GetKey("right"))             { GetComponent<SpriteRenderer>().sprite = right; }
+         if (Input.GetKey("down"))              { GetComponent<SpriteRenderer>().sprite = down; }
+         if (Input.GetKey("up"))                { GetComponent<SpriteRenderer>().sprite = up; }
+
+        //Attack Direction
+         if (GetComponent<SpriteRenderer>().sprite == left && Input.GetKey("space")) { GetComponent<SpriteRenderer>().sprite = attk_left; }
+         if (GetComponent<SpriteRenderer>().sprite == right && Input.GetKey("space")) { GetComponent<SpriteRenderer>().sprite = attk_right; }
+         if (GetComponent<SpriteRenderer>().sprite == down && Input.GetKey("space")) { GetComponent<SpriteRenderer>().sprite = attk_down; }
+         if (GetComponent<SpriteRenderer>().sprite == up && Input.GetKey("space")) { GetComponent<SpriteRenderer>().sprite = attk_up; }
+
+
          if (Input.GetKeyDown(KeyCode.B))
          {
              inventory.Open();
@@ -60,8 +75,14 @@ public class PlayerManager : MonoBehaviour {
         {
             inventory.AddItem(collision.gameObject.GetComponent<Item>()); //Adds the item to the inventory.
 
-            Destroy(collision.gameObject);
+           Destroy(collision.gameObject);
         }
+
+        //if (collision.gameObject.tag == "Enemy" && Input.GetKeyDown("space"))
+        //{
+
+        //    Destroy(collision.gameObject);
+        //}
     }
 
     void FixedUpdate() {
--- Assets/Scripts/enemyManager.cs
+++ Assets/Scripts/enemyManager.cs
@@ -29,7 +29,7 @@ public class enemyManager : MonoBehaviour {
         Vector2 moveDirection = GetComponent<Rigidbody2D>().velocity;
         if (moveDirection != Vector2.zero) {
             float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            //transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         }
         if (moveDirection.x < 0) {
             GetComponent<SpriteRenderer>().sprite = left;
